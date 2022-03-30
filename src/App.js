03059e162f96a1f230f0dcff38e15e663fb7f9f5
SeonHakJi <jshag90@naver.com>
2022-03-30 23:57:40 +0900

    [Feat] Hooks도입

--- src/App.js
+++ src/App.js
@@ -1,8 +1,96 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { React, useState } from "react";
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
 
 function App() {
-  return <div className="App">Hello React!!!</div>;
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>;
+  } else if (mode === "READ") {
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+      }
+    }
+    content = <Article title="Read" body="Hello, Read"></Article>;
+  }
+  return (
+    <div>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
+    </div>
+  );
 }
 
 export default App;
